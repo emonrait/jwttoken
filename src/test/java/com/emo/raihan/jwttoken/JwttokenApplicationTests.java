c38e4ab049b8dd3de6e82591fa60e28517e76ed7
emonrait <emonrait@gmail.com>
2022-01-03 23:59:00 +0600

    first commit

---
+++ src/test/java/com/emo/raihan/jwttoken/JwttokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emo.raihan.jwttoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwttokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
